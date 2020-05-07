0e8e34709bbaef911ad025791873bb4ed5129a81
arek <bombarder1118@o2.pl>
2020-05-07 14:46:39 +0200

    updated script

--- run_skiba_arkadiusz.sh
+++ run_skiba_arkadiusz.sh
@@ -1,1 +1,20 @@
-/* todo */
+case "$1" in
+   "clone")
+	 git clone https://github.com/Arekss/projekt.git
+   ;;
+   "clean")
+	 sudo rm -Rf projekt
+	 sudo rm -Rf updatedScript
+   ;;
+   "update")
+	 git clone https://github.com/Arekss/updatedScript.git
+   ;;
+  "run")
+ 	if [[ -d "projekt" ]]
+  	then
+        cd projekt
+  	fi
+  	docker build -t konteneras:1.0 .
+  	docker run -it konteneras:1.0
+   ;;
+esac
